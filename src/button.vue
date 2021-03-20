ad7dec4773ba0ac8132660440beb5fd262da8654
xiaoxue0423 <xiaoxuexue19980423@163.com>
2021-03-20 18:08:46 +0800

    添加icon和iconPosition属性

--- src/button.vue
+++ src/button.vue
@@ -1,16 +1,23 @@
 <template>
-  <div>
-    <button class="g-button">hello</button>
-  </div>
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+      <svg v-if="icon" class="icon">
+        <use :xlink:href=`#i-${icon}`></use>
+      </svg>
+      <div class="content">
+        <slot></slot>
+      </div>
+    </button>
 </template>
 
 <script>
 export default {
-  name: "button"
+  name: "button",
+  props:['icon','iconPosition']
 }
 </script>
 
 <style scoped lang="scss">
+
 .g-button{
   height: var(--button-height);
   font-size: var(--font-size);
@@ -18,6 +25,27 @@ export default {
   border-radius: var(--border-radius);
   border: 1px solid var(--border-color);
   background: var(--button-bg);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  vertical-align: middle;
+  >.icon {
+    order: 1;
+    margin-right: .3em;
+  }
+  >.content {
+    order: 2;
+  }
+  &.icon-right{
+    >.icon {
+      order: 2;
+      margin-left: .3em;
+      margin-right: 0;
+    }
+    >.content{
+      order: 1;
+    }
+  }
   &:hover {
     border-color: var(--border-color-hover);
   }
